82a7882dc86459f8b85e4a3be8e4799fde8169fd
luisurrea2016# <luis.urrea@motortrak.com>
2018-10-13 14:58:00 +0100

    Add forecast mock data

---
+++ src/mockData.js
@@ -0,0 +1,367 @@
+export const mondayList = [
+  {
+    dt: 1538524800,
+    main: {
+      temp: 7.11,
+      temp_min: 6.98,
+      temp_max: 7.11,
+      pressure: 1009.19,
+      sea_level: 1034.77,
+      grnd_level: 1009.19,
+      humidity: 79,
+      temp_kf: 0.14,
+    },
+    weather: [
+      {
+        id: 801,
+        main: 'Clouds',
+        description: 'few clouds',
+        icon: '02n',
+      },
+    ],
+    clouds: {
+      all: 24,
+    },
+    wind: {
+      speed: 4.82,
+      deg: 286.002,
+    },
+    rain: {},
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-15 00:00:00',
+  },
+  {
+    dt: 1538535600,
+    main: {
+      temp: 6.21,
+      temp_min: 6.12,
+      temp_max: 6.21,
+      pressure: 1009.73,
+      sea_level: 1035.4,
+      grnd_level: 1009.73,
+      humidity: 85,
+      temp_kf: 0.09,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10n',
+      },
+    ],
+    clouds: {
+      all: 64,
+    },
+    wind: {
+      speed: 2.67,
+      deg: 272.501,
+    },
+    rain: {
+      '3h': 0.03,
+    },
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-15 03:00:00',
+  },
+  {
+    dt: 1538546400,
+    main: {
+      temp: 6.32,
+      temp_min: 6.27,
+      temp_max: 6.32,
+      pressure: 1010.25,
+      sea_level: 1035.84,
+      grnd_level: 1010.25,
+      humidity: 95,
+      temp_kf: 0.05,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10n',
+      },
+    ],
+    clouds: {
+      all: 92,
+    },
+    wind: {
+      speed: 1.71,
+      deg: 233.004,
+    },
+    rain: {
+      '3h': 0.47,
+    },
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-15 06:00:00',
+  },
+  {
+    dt: 1538557200,
+    main: {
+      temp: 8.02,
+      temp_min: 8.02,
+      temp_max: 8.02,
+      pressure: 1010.1,
+      sea_level: 1035.68,
+      grnd_level: 1010.1,
+      humidity: 97,
+      temp_kf: 0,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10d',
+      },
+    ],
+    clouds: {
+      all: 92,
+    },
+    wind: {
+      speed: 1.86,
+      deg: 171.004,
+    },
+    rain: {
+      '3h': 0.42,
+    },
+    sys: {
+      pod: 'd',
+    },
+    dt_txt: '2018-10-15 09:00:00',
+  },
+  {
+    dt: 1538568000,
+    main: {
+      temp: 12.09,
+      temp_min: 12.09,
+      temp_max: 12.09,
+      pressure: 1009.46,
+      sea_level: 1034.9,
+      grnd_level: 1009.46,
+      humidity: 90,
+      temp_kf: 0,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10d',
+      },
+    ],
+    clouds: {
+      all: 88,
+    },
+    wind: {
+      speed: 4.77,
+      deg: 246.502,
+    },
+    rain: {
+      '3h': 0.225,
+    },
+    sys: {
+      pod: 'd',
+    },
+    dt_txt: '2018-10-15 12:00:00',
+  },
+];
+
+export const tuesdayList = [
+  {
+    dt: 1538524800,
+    main: {
+      temp: 7.11,
+      temp_min: 5.01,
+      temp_max: 7.11,
+      pressure: 1009.19,
+      sea_level: 1034.77,
+      grnd_level: 1009.19,
+      humidity: 79,
+      temp_kf: 0.14,
+    },
+    weather: [
+      {
+        id: 801,
+        main: 'Clouds',
+        description: 'few clouds',
+        icon: '02n',
+      },
+    ],
+    clouds: {
+      all: 24,
+    },
+    wind: {
+      speed: 4.82,
+      deg: 286.002,
+    },
+    rain: {},
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-16 00:00:00',
+  },
+  {
+    dt: 1538535600,
+    main: {
+      temp: 6.21,
+      temp_min: 6.12,
+      temp_max: 6.21,
+      pressure: 1009.73,
+      sea_level: 1035.4,
+      grnd_level: 1009.73,
+      humidity: 85,
+      temp_kf: 0.09,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10n',
+      },
+    ],
+    clouds: {
+      all: 64,
+    },
+    wind: {
+      speed: 2.67,
+      deg: 272.501,
+    },
+    rain: {
+      '3h': 0.03,
+    },
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-16 03:00:00',
+  },
+  {
+    dt: 1538546400,
+    main: {
+      temp: 6.32,
+      temp_min: 6.27,
+      temp_max: 6.32,
+      pressure: 1010.25,
+      sea_level: 1035.84,
+      grnd_level: 1010.25,
+      humidity: 95,
+      temp_kf: 0.05,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10n',
+      },
+    ],
+    clouds: {
+      all: 92,
+    },
+    wind: {
+      speed: 1.71,
+      deg: 233.004,
+    },
+    rain: {
+      '3h': 0.47,
+    },
+    sys: {
+      pod: 'n',
+    },
+    dt_txt: '2018-10-16 06:00:00',
+  },
+  {
+    dt: 1538557200,
+    main: {
+      temp: 8.02,
+      temp_min: 8.02,
+      temp_max: 25.14,
+      pressure: 1010.1,
+      sea_level: 1035.68,
+      grnd_level: 1010.1,
+      humidity: 97,
+      temp_kf: 0,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10d',
+      },
+    ],
+    clouds: {
+      all: 92,
+    },
+    wind: {
+      speed: 1.86,
+      deg: 171.004,
+    },
+    rain: {
+      '3h': 0.42,
+    },
+    sys: {
+      pod: 'd',
+    },
+    dt_txt: '2018-10-16 09:00:00',
+  },
+  {
+    dt: 1538568000,
+    main: {
+      temp: 12.09,
+      temp_min: 12.09,
+      temp_max: 12.09,
+      pressure: 1009.46,
+      sea_level: 1034.9,
+      grnd_level: 1009.46,
+      humidity: 90,
+      temp_kf: 0,
+    },
+    weather: [
+      {
+        id: 500,
+        main: 'Rain',
+        description: 'light rain',
+        icon: '10d',
+      },
+    ],
+    clouds: {
+      all: 88,
+    },
+    wind: {
+      speed: 4.77,
+      deg: 246.502,
+    },
+    rain: {
+      '3h': 0.225,
+    },
+    sys: {
+      pod: 'd',
+    },
+    dt_txt: '2018-10-16 12:00:00',
+  },
+];
+
+export const forecast = {
+  cod: '200',
+  message: 0.0032,
+  cnt: 10,
+  list: [...mondayList, ...tuesdayList],
+  city: {
+    id: 3333229,
+    name: 'City of Edinburgh',
+    coord: {
+      lat: 55.9497,
+      lon: -3.1934,
+    },
+    country: 'GB',
+  },
+};
