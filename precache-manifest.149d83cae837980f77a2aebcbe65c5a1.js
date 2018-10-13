08837772f30f24cdb6b9e701c62dd1a3c14354f2
archimedes25 <archimedes25@outlook.com>
2018-10-13 17:03:37 +0000

    Updates

---
+++ precache-manifest.149d83cae837980f77a2aebcbe65c5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "fe8a25c9331d719f7adb",
+    "url": "/weather-app/static/js/main.fe8a25c9.chunk.js"
+  },
+  {
+    "revision": "1c27f58c8882ddfafdb5",
+    "url": "/weather-app/static/js/1.1c27f58c.chunk.js"
+  },
+  {
+    "revision": "fe8a25c9331d719f7adb",
+    "url": "/weather-app/static/css/main.4c83967c.chunk.css"
+  },
+  {
+    "revision": "8b9b83bac0d4c2076d9ddde54fc5b7b6",
+    "url": "/weather-app/index.html"
+  }
+];
